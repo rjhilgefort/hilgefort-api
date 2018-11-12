c6b391fdbbb1e5597c073eeb2e53aba0943dab7e
Rob Hilgefort <rjhilgefort@gmail.com>
2018-11-11 23:03:49 -0700

    Added email and better error reporter

--- src/server/routes/contact.ts
+++ src/server/routes/contact.ts
@@ -3,7 +3,7 @@ import * as t from 'io-ts'
 import * as R from 'ramda'
 import dedent from 'dedent'
 import env from '../../env'
-import { PathReporter } from '../../lib/reporters'
+import { HumanReporter } from '../../lib/reporters'
 
 const {
   SMTP_TO_ADDRESS
@@ -15,25 +15,28 @@ const EmailRequest = R.compose(
   t.exact,
   t.type,
 )({
-  from: t.string,
-  body: t.string,
+  email: t.string,
+  phone: t.string,
+  message: t.string,
 })
 
 router.post('/', (req: Request, res: Response) => {
   EmailRequest.decode(req.body).fold(
     R.compose(
       res.locals.response.bad_request,
-      PathReporter.failure,
+      HumanReporter.failure,
     ),
-    ({ from, body }) => {
+    ({ email, phone, message }) => {
       return res.locals.smtp.sendMail({
-        from,
+        // TODO: Can this line be removed?
+        from: email,
         to: SMTP_TO_ADDRESS,
-        subject: `Website Inquiry From: ${req.hostname}`,
+        subject: `Website Inquiry For: ${req.hostname}`,
         text: dedent`
           WEBSITE: ${req.hostname}
-          FROM:    ${from}
-          MESSAGE: ${body}
+          EMAIL:   ${email}
+          PHONE:   ${phone}
+          MESSAGE: ${message}
         `,
       })
         .then(res.locals.response.success, res.locals.response.bad_request)
